d29429d140ee15b27ec3a3ff10d040b0d12c804c
Alex Horton <alexhortonmusic@gmail.com>
2016-06-06 11:48:38 -0500

    first completed quiz draft

--- food.js
+++ food.js
@@ -4,33 +4,49 @@
 var dogContainer = document.getElementById('dogContainer');
 var catContainer = document.getElementById('catContainer')
 
-//--------Food Functions-----//
-
-
-
-
 //----------Functions for event listeners----//
 
 function executeCodeWhenDogLoads () {
   var dogFood = JSON.parse(event.target.responseText);
   console.log("dogFood", dogFood); 
   
+  // Food function that prints food info to container
+  // var i += 
   dogFood.dog_brands.forEach(function (foods) {
     dogContainer.innerHTML += `
       <h2>${foods.name}</h2>
         <p><b>${foods.types[0].type}</b></p>
-          <p>Bag Size:${foods.types[0].volumes[0]}
+          <p>Bag Size:</p>
+            <p>${foods.types[0].volumes[0].name}: $${foods.types[0].volumes[0].price}</p>
+            <p>${foods.types[0].volumes[1].name}: $${foods.types[0].volumes[1].price}</p>
         <p><b>${foods.types[1].type}</b></p>
-          <p>Bag Size:${foods.types[1].volumes[1].stringify}
+          <p>Bag Size:</p>
+            <p>${foods.types[1].volumes[0].name}: $${foods.types[0].volumes[0].price}</p>
+            <p>${foods.types[1].volumes[1].name}: $${foods.types[1].volumes[1].price}</p>
     `;
   });
-
-
 };
 
 function executeCodeWhenCatLoads () {
   var catFood = JSON.parse(event.target.responseText);
   console.log("catFood", catFood)
+
+  // Food function that prints food info to container
+  // var i += 
+  catFood.cat_brands.forEach(function (foods) {
+    catContainer.innerHTML += `
+      <h2>${foods.name}</h2>
+        <p><b>${foods.types[0].type}</b></p>
+          <p>Bag Size:</p>
+            <p>${foods.types[0].volumes[0].name}: $${foods.types[0].volumes[0].price}</p>
+            <p>${foods.types[0].volumes[1].name}: $${foods.types[0].volumes[1].price}</p>
+            <p>${foods.types[0].volumes[2].name}: $${foods.types[0].volumes[2].price}</p>
+        <p><b>${foods.types[1].type}</b></p>
+          <p>Bag Size:</p>
+            <p>${foods.types[1].volumes[0].name}: $${foods.types[0].volumes[0].price}</p>
+            <p>${foods.types[1].volumes[1].name}: $${foods.types[1].volumes[1].price}</p>
+    `;
+  });
 };
 
 function executeIfFilesFailToLoad () {
@@ -52,12 +68,13 @@ dogRequest.open('GET', 'dogFood.json');
 dogRequest.send();
 
 
-// // Cat XHR
-// var catRequest = new XMLHttpRequest();
+// Cat XHR
+var catRequest = new XMLHttpRequest();
 
-// catRequest.addEventListener('load', executeCodeWhenCatLoads);
-// catRequest.addEventListener('error', executeIfFilesFailToLoad);
+catRequest.addEventListener('load', executeCodeWhenCatLoads);
+catRequest.addEventListener('error', executeIfFilesFailToLoad);
 
-// catRequest.open('GET', 'catFood.json');
+catRequest.open('GET', 'catFood.json');
 
-// catRequest.send();
+catRequest.send();
+
