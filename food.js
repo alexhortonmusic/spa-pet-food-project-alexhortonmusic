0a6e8de14e102e4f637628518f89b245107d8c65
Alex Horton <alexhortonmusic@gmail.com>
2016-06-10 12:01:31 -0500

    js now written with for loops/completed all criteria

--- food.js
+++ food.js
@@ -1,60 +1,75 @@
 
 //---------Containers---------//
 
+var container = document.getElementById('container');
 var dogContainer = document.getElementById('dogContainer');
-var catContainer = document.getElementById('catContainer')
+var catContainer = document.getElementById('catContainer');
 
 //----------Functions for event listeners----//
+
+function dogPrint (dogJSON) {
+  for (var i = 0; i < dogJSON.dog_brands.length; i++) {
+    var dogBrand = dogJSON.dog_brands[i];
+    var brandName = dogBrand.name;
+    dogContainer.innerHTML += `<h2>${brandName}</h2>`;
+    for (var j = 0; j < dogBrand.types.length; j++) {
+      var foodTypes = dogBrand.types[j];
+      var qualityType = foodTypes.type;
+      dogContainer.innerHTML += `<p class="qualityType">${qualityType}</p>`;
+      for (var k = 0; k < foodTypes.volumes.length; k++) {
+        var foodVolumes = foodTypes.volumes[k];
+        var volumeName = foodVolumes.name;
+        var volumePrice = foodVolumes.price;
+        dogContainer.innerHTML += `
+          <p>Bag Size:</p>
+          <p>${volumeName}: $${volumePrice}</p>
+        `;
+      };   
+    };
+  };
+};
 
 function executeCodeWhenDogLoads () {
   var dogFood = JSON.parse(event.target.responseText);
   console.log("dogFood", dogFood); 
-  
-  // Food function that prints food info to container
-  
-  dogFood.dog_brands.forEach(function (foods) {
-    dogContainer.innerHTML += `
-      <div class="dogBox">
-        <h2>${foods.name}</h2>
-          <p><b>${foods.types[0].type}</b></p>
-            <p>Bag Size:</p>
-              <p>${foods.types[0].volumes[0].name}: $${foods.types[0].volumes[0].price}</p>
-              <p>${foods.types[0].volumes[1].name}: $${foods.types[0].volumes[1].price}</p>
-          <p><b>${foods.types[1].type}</b></p>
-            <p>Bag Size:</p>
-              <p>${foods.types[1].volumes[0].name}: $${foods.types[1].volumes[0].price}</p>
-              <p>${foods.types[1].volumes[1].name}: $${foods.types[1].volumes[1].price}</p>
-      </div>
-    `;
-  });
+  dogPrint(dogFood);
 };
+  
+
+function catPrint (catJSON) {
+  for (var i = 0; i < catJSON.cat_brands.length; i++) {
+    var catBrand = catJSON.cat_brands[i];
+    var brandName = catBrand.name;
+    var catBreeds = catBrand.breeds;
+    catContainer.innerHTML += `<h2>${brandName}</h2>`;
+    catContainer.innerHTML += `<h3>Suggested for these breeds:<br>${catBreeds}</h3>`;
+    for (var j = 0; j < catBrand.types.length; j++) {
+      var foodTypes = catBrand.types[j];
+      var qualityType = foodTypes.type;
+      catContainer.innerHTML += `<p class="qualityType">${qualityType}</p>`;
+      for (var k = 0; k < foodTypes.volumes.length; k++) {
+        var foodVolumes = foodTypes.volumes[k];
+        var volumeName = foodVolumes.name;
+        var volumePrice = foodVolumes.price;
+        catContainer.innerHTML += `
+          <p>Bag Size:</p>
+          <p>${volumeName}: $${volumePrice}</p>
+        `;
+      }
+    }
+  }
+}
+
 
 function executeCodeWhenCatLoads () {
   var catFood = JSON.parse(event.target.responseText);
   console.log("catFood", catFood)
-
-  // Food function that prints food info to container
-  catFood.cat_brands.forEach(function (foods) {
-    catContainer.innerHTML += `
-      <div class="catBox">
-        <h2>${foods.name}</h2>
-          <h3>For breeds: ${foods.breeds}</h3>
-          <p><b>${foods.types[0].type}</b></p>
-            <p>Bag Size:</p>
-              <p>${foods.types[0].volumes[0].name}: $${foods.types[0].volumes[0].price}</p>
-              <p>${foods.types[0].volumes[1].name}: $${foods.types[0].volumes[1].price}</p>
-          <p><b>${foods.types[1].type}</b></p>
-            <p>Bag Size:</p>
-              <p>${foods.types[1].volumes[0].name}: $${foods.types[1].volumes[0].price}</p>
-              <p>${foods.types[1].volumes[1].name}: $${foods.types[1].volumes[1].price}</p>
-      </div>
-    `;
-  });
-  
+  catPrint(catFood);
 };
 
+
 function executeIfFilesFailToLoad () {
-  dogContainer.innerHTML = ('error');
+  container.innerHTML = ('error');
 };
 
 
